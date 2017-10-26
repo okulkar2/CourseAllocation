6c209cc515fc53d46923335f2d75c5ec9498ea41
okulkar2 <omkr22@gmail.com>
2017-10-26 11:48:28 -0700

    Initial Front End Files Created

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [
+    AppComponent,
+    HeaderComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
--- public/src/app/header/header.component.ts
+++ public/src/app/header/header.component.ts
@@ -1,11 +1,11 @@
-import { Component, Output, EventEmitter } from '@angular/core'
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 
-export class HeaderComponent{
+export class HeaderComponent {
 
     @Output() featureClicked = new EventEmitter<string>();
 
